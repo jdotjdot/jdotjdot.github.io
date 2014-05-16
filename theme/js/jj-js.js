118ebd0c08d9f386893e9427f1cbb9611d654b05
JJ <JJ@jdotjdot.com>
2014-05-16 00:40:28 -0400

    Published on 2014-05-16T00:40:27.862959 by Fabric

---
+++ theme/js/jj-js.js
@@ -0,0 +1,10 @@
+//;
+//
+//var socialSpinner = function() {$(".social-icon").hover(function() {
+//        $(this).toggleClass("fa-spin", true);
+//    }, function() {
+//        $(this).toggleClass("fa-spin", false);
+//    });
+//};
+//
+//$(document).ready(socialSpinner);
